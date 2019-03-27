6abef3f273a8bd71b5dbb41990f6c80d36a2b3d5
Tulio Carvalho <tuliomscarvalho@gmail.com>
2019-03-27 17:01:23 -0300

    Updates

---
+++ precache-manifest.5482f8a7a24b4ae1d5a0fe03ece34b7e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/aprovisionamento/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "46acd036a9c7ac41796b",
+    "url": "/aprovisionamento/static/css/main.364ddcda.chunk.css"
+  },
+  {
+    "revision": "0eaa21d46e663bb99842",
+    "url": "/aprovisionamento/static/js/runtime~main.6a265da4.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/aprovisionamento/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "31852c7ab22c293990f4",
+    "url": "/aprovisionamento/static/js/2.365f1bb8.chunk.js"
+  },
+  {
+    "revision": "a5bb7cca967f179c6e7c5d4c60a54259",
+    "url": "/aprovisionamento/static/media/logoviareal.a5bb7cca.png"
+  },
+  {
+    "revision": "46acd036a9c7ac41796b",
+    "url": "/aprovisionamento/static/js/main.32e638f4.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/aprovisionamento/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/aprovisionamento/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/aprovisionamento/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "31852c7ab22c293990f4",
+    "url": "/aprovisionamento/static/css/2.ca508a53.chunk.css"
+  },
+  {
+    "revision": "7162639cd6cc56f9eac42fab5e9324b5",
+    "url": "/aprovisionamento/index.html"
+  }
+];
